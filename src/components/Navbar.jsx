7781722c9f12544a6b9054e4511daa4ecf7c9296
Esteban5957L <4estebanvillalba20@gmail.com>
2023-02-04 21:11:10 -0300

    NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={Logo_UTN} alt="hoobank" className="w-[124px] h-[32px]" />
+      <img src={Logo_UTN} alt="hoobank" className="w-[51px] h-[50px] bg-white rounded-full p-px" />
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
